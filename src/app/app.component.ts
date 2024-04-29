b9acfe580c7a6440b6c87433f21d9317cf01a878
MihirMendse <mihiram515@gmail.com>
2024-04-29 15:51:30 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface SubmittedDataType {
+  email: string;
+  question: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'mfe2';
+  form!: FormGroup;
+  submittedData: SubmittedDataType[] = [];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      question: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+
+  onSubmit() {
+    if (this.form.valid) {
+      const email = this.form.get('email')?.value;
+      const question = this.form.get('question')?.value;
+      this.submittedData.push(this.form.value);
+      this.form.reset();
+
+      console.log('Email:', email);
+      console.log('Question:', question);
+
+      console.log('Complete Data:', this.submittedData);
+      
+    }
+  }
 }
